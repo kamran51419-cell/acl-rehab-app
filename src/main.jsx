2ea02732fb9b1e81248975c47a7f941be68dee37
kamran51419-cell <kamran51419@gmail.com>
2026-07-21 12:25:40 +0100

    Install workout flow UI fixes

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 import App from './App.jsx'
 import { installUiRuntimeFixes } from './uiRuntimeFixes'
 import { installProgrammeButtonLabels } from './programmeButtonLabels'
+import { installWorkoutFlowUiFixes } from './workoutFlowUiFixes'
 
 const updateSW = registerSW({
   immediate: true,
@@ -15,6 +16,7 @@ const updateSW = registerSW({
 
 installUiRuntimeFixes()
 installProgrammeButtonLabels()
+installWorkoutFlowUiFixes()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>

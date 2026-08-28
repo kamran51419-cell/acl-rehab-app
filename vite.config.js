568138989bb9032383bd30cc7304d8433551ff7c
kamran51419-cell <kamran51419@gmail.com>
2026-08-28 11:45:36 +0100

    Enable whole-card dragging

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ import { workoutExerciseEditBuildPlugin } from './src/workoutExerciseEditBuildPl
 import { workoutUiPolishBuildPlugin } from './src/workoutUiPolishBuildPlugin.js'
 import { exercisePickerConsistencyBuildPlugin } from './src/exercisePickerConsistencyBuildPlugin.js'
 import { reorderUxBuildPlugin } from './src/reorderUxBuildPlugin.js'
+import { wholeCardDragBuildPlugin } from './src/wholeCardDragBuildPlugin.js'
 import { repeatedIntervalBlocksPlugin } from './src/repeatedIntervalBlocksPlugin.js'
 import { verticalIntervalWorkoutDisplayPlugin } from './src/verticalIntervalWorkoutDisplayPlugin.js'
 
@@ -21,6 +22,7 @@ export default defineConfig({
     verticalIntervalWorkoutDisplayPlugin(),
     exercisePickerConsistencyBuildPlugin(),
     reorderUxBuildPlugin(),
+    wholeCardDragBuildPlugin(),
     react(),
     tailwindcss(),
     VitePWA({

b97931f313389febe360cff9bef2829578efdb8f
kamran51419-cell <kamran51419@gmail.com>
2026-08-29 15:13:53 +0100

    Apply workout actions polish after workout transforms

--- vite.config.js
+++ vite.config.js
@@ -25,6 +25,7 @@ import { seamlessTabsBuildPlugin } from './src/seamlessTabsBuildPlugin.js'
 import { workoutCardAlignmentBuildPlugin } from './src/workoutCardAlignmentBuildPlugin.js'
 import { setFieldFloatingLabelsBuildPlugin } from './src/setFieldFloatingLabelsBuildPlugin.js'
 import { exerciseMetaHierarchyBuildPlugin } from './src/exerciseMetaHierarchyBuildPlugin.js'
+import { workoutActionsModalPolishBuildPlugin } from './src/workoutActionsModalPolishBuildPlugin.js'
 import { repeatedIntervalBlocksPlugin } from './src/repeatedIntervalBlocksPlugin.js'
 import { intervalSummaryGrammarBuildPlugin } from './src/intervalSummaryGrammarBuildPlugin.js'
 import { verticalIntervalWorkoutDisplayPlugin } from './src/verticalIntervalWorkoutDisplayPlugin.js'
@@ -57,6 +58,7 @@ export default defineConfig({
     workoutCardAlignmentBuildPlugin(),
     setFieldFloatingLabelsBuildPlugin(),
     exerciseMetaHierarchyBuildPlugin(),
+    workoutActionsModalPolishBuildPlugin(),
     react(),
     tailwindcss(),
     VitePWA({

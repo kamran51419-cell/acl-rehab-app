7baaa715c1ee23fb896a999e69760a86fe3c08b3
kamran51419-cell <kamran51419@gmail.com>
2026-08-30 12:04:04 +0100

    Wire unified add exercise state fix

--- vite.config.js
+++ vite.config.js
@@ -30,6 +30,7 @@ import { repeatedIntervalBlocksPlugin } from './src/repeatedIntervalBlocksPlugin
 import { intervalSummaryGrammarBuildPlugin } from './src/intervalSummaryGrammarBuildPlugin.js'
 import { verticalIntervalWorkoutDisplayPlugin } from './src/verticalIntervalWorkoutDisplayPlugin.js'
 import { unifiedAddExerciseSetupBuildPlugin } from './src/unifiedAddExerciseSetupBuildPlugin.js'
+import { unifiedAddExerciseSetupStateFixBuildPlugin } from './src/unifiedAddExerciseSetupStateFixBuildPlugin.js'
 
 export default defineConfig({
   plugins: [
@@ -61,6 +62,7 @@ export default defineConfig({
     exerciseMetaHierarchyBuildPlugin(),
     workoutActionTextBuildPlugin(),
     unifiedAddExerciseSetupBuildPlugin(),
+    unifiedAddExerciseSetupStateFixBuildPlugin(),
     react(),
     tailwindcss(),
     VitePWA({
